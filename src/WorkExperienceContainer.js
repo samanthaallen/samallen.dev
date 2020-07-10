173b2df76b585300ce97620f178d7d7fd9ee734e
samanthaallen <allen.c.samantha@gmail.com>
2020-07-09 19:48:34 -0700

    Adding padding to WorkExperienceContainer

--- src/WorkExperienceContainer.js
+++ src/WorkExperienceContainer.js
@@ -16,65 +16,67 @@ import JobDescriptionUT from "./JobDescriptionUT";
 function WorkExperienceContainer() {
   return (
     <PostContainer stickyTape="Experience">
-      <WorkExperienceLayout
-        company="Intercom"
-        jobTitle="Product Enginer III"
-        jobDate="January 2020 - August 2020"
-        jobDescription={<JobDescriptionIntercom />}
-      />
-      <hr className="WorkExperienceHr" />
-      <WorkExperienceLayout
-        company="Salesforce"
-        jobTitle="Lead Software Engineer"
-        jobDate="January 2019 - September 2019"
-        jobDescription={<JobDescriptionSalesforceLMTS />}
-      />
-      <hr className="SalesforceHr" />
-      <WorkExperienceLayout
-        jobTitle="Senior Software Engineer"
-        jobDate="June 2017 - January 2019"
-        jobDescription={<JobDescriptionSalesforceSMTS />}
-      />
-      <hr className="SalesforceHr" />
-      <WorkExperienceLayout
-        jobTitle="Software Engineer"
-        jobDate="April 2016 - June 2017"
-        jobDescription={<JobDescriptionSalesforceMTS />}
-      />
-      <hr className="SalesforceHr" />
-      <WorkExperienceLayout
-        jobTitle="Associate Quality Engineer"
-        jobDate="February 2015 - March 2016"
-        jobDescription={<JobDescriptionSalesforceAMTS />}
-      />
-      <hr className="WorkExperienceHr" />
-      <WorkExperienceLayout
-        company="UnboundID"
-        jobTitle="Associate Quality Assurance Engineer"
-        jobDate="August 2013 - December 2014"
-        jobDescription={<JobDescriptionUnboundID />}
-      />
-      <hr className="WorkExperienceHr" />
-      <WorkExperienceLayout
-        company="HP"
-        jobTitle="Software Engineer Intern"
-        jobDate="May 2014 - August 2014"
-        jobDescription={<JobDescriptionHP />}
-      />
-      <hr className="WorkExperienceHr" />
-      <WorkExperienceLayout
-        company="Etherios"
-        jobTitle="Salesforce Technical Consulting Intern"
-        jobDate="May 2013 - August 2013"
-        jobDescription={<JobDescriptionEtherios />}
-      />
-      <hr className="WorkExperienceHr" />
-      <WorkExperienceLayout
-        company="University of Texas Applied Research Labs"
-        jobTitle="Student Technician"
-        jobDate="January 2013 - May 2013"
-        jobDescription={<JobDescriptionUT />}
-      />
+      <div className="WorkExperienceContainer">
+        <WorkExperienceLayout
+          company="Intercom"
+          jobTitle="Product Enginer III"
+          jobDate="January 2020 - August 2020"
+          jobDescription={<JobDescriptionIntercom />}
+        />
+        <hr className="WorkExperienceHr" />
+        <WorkExperienceLayout
+          company="Salesforce"
+          jobTitle="Lead Software Engineer"
+          jobDate="January 2019 - September 2019"
+          jobDescription={<JobDescriptionSalesforceLMTS />}
+        />
+        <hr className="SalesforceHr" />
+        <WorkExperienceLayout
+          jobTitle="Senior Software Engineer"
+          jobDate="June 2017 - January 2019"
+          jobDescription={<JobDescriptionSalesforceSMTS />}
+        />
+        <hr className="SalesforceHr" />
+        <WorkExperienceLayout
+          jobTitle="Software Engineer"
+          jobDate="April 2016 - June 2017"
+          jobDescription={<JobDescriptionSalesforceMTS />}
+        />
+        <hr className="SalesforceHr" />
+        <WorkExperienceLayout
+          jobTitle="Associate Quality Engineer"
+          jobDate="February 2015 - March 2016"
+          jobDescription={<JobDescriptionSalesforceAMTS />}
+        />
+        <hr className="WorkExperienceHr" />
+        <WorkExperienceLayout
+          company="UnboundID"
+          jobTitle="Associate Quality Assurance Engineer"
+          jobDate="August 2013 - December 2014"
+          jobDescription={<JobDescriptionUnboundID />}
+        />
+        <hr className="WorkExperienceHr" />
+        <WorkExperienceLayout
+          company="HP"
+          jobTitle="Software Engineer Intern"
+          jobDate="May 2014 - August 2014"
+          jobDescription={<JobDescriptionHP />}
+        />
+        <hr className="WorkExperienceHr" />
+        <WorkExperienceLayout
+          company="Etherios"
+          jobTitle="Salesforce Technical Consulting Intern"
+          jobDate="May 2013 - August 2013"
+          jobDescription={<JobDescriptionEtherios />}
+        />
+        <hr className="WorkExperienceHr" />
+        <WorkExperienceLayout
+          company="University of Texas Applied Research Labs"
+          jobTitle="Student Technician"
+          jobDate="January 2013 - May 2013"
+          jobDescription={<JobDescriptionUT />}
+        />
+      </div>
     </PostContainer>
   );
 }
