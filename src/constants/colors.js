04a234bbb74ec2a66636e8be97b851b8551bdb1e
samanthaallen <allen.c.samantha@gmail.com>
2020-07-26 16:57:38 -0700

    adding reds

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -2,5 +2,7 @@ const lightPurple = "#e4dcec";
 const darkPurple = "#A16FBF";
 const lightGreen = "#DEEDEB";
 const darkGreen = "#71BFB2";
+const lightRed = "#EDE2DD";
+const darkRed = "#BF806F";
 
-export { lightPurple, darkPurple, lightGreen, darkGreen };
+export { lightPurple, darkPurple, lightGreen, darkGreen, lightRed, darkRed };
