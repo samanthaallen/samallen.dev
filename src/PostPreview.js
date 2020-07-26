fafb9e80873457c4d6829658fd754beb1272c05b
samanthaallen <allen.c.samantha@gmail.com>
2020-07-26 15:20:31 -0700

    Filling in tech blog

--- src/PostPreview.js
+++ src/PostPreview.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./PostPreview.css";
 
-function PostPreview() {
-    return(<div></div>);
+function PostPreview(props) {
+  return (
+    <div className="PostPreview">
+      <a className="PostText" href={props.url}>
+        <div className="PostSpacer">
+          <h2>{props.title}</h2>
+        </div>
+        <img src={props.image} className="PostImage" />
+        <p>{props.children}</p>
+      </a>
+    </div>
+  );
 }
 
 export default PostPreview;
--- src/TechPage.js
+++ src/TechPage.js
@@ -12,10 +12,44 @@ function TechPage() {
       stickyTapeColor={darkGreen}
     >
       <TechPostLayout>
-        <PostPreview></PostPreview>
-        <PostPreview></PostPreview>
-        <PostPreview></PostPreview>
-        <PostPreview></PostPreview>
+        <PostPreview
+          title="Get Hired: Handling Object-Oriented Design Questions"
+          url="https://dev.to/techdebtor/get-hired-handling-object-oriented-design-questions-4i70"
+          image="https://res.cloudinary.com/practicaldev/image/fetch/s--5fU_jn_H--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/g4te7rpeeltt4miw93yr.jpg"
+        >
+          Let's work through an object-oriented design question that I was asked
+          in an interview with a major tech company.
+          <strong> Read more →</strong>
+        </PostPreview>
+        <PostPreview
+          title="Get Hired: Behavioral Interviews for Devs"
+          url="https://dev.to/techdebtor/get-hired-behavioral-interviews-for-devs-1f97"
+          image="https://res.cloudinary.com/practicaldev/image/fetch/s--zMskubs_--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/zp8uuom8yxexc2oo7whu.jpeg"
+        >
+          I learned the hard way: you need to prepare for your non-technical
+          interviews too.
+          <strong> Read more →</strong>
+        </PostPreview>
+        <PostPreview
+          title="Get Hired: Data Structure and Algorithm Interviews For People Who Hate Them"
+          url="https://dev.to/techdebtor/get-hired-data-structure-and-algorithm-interviews-for-people-who-hate-them-32ga"
+          image="https://res.cloudinary.com/practicaldev/image/fetch/s--Pwk3M5Sr--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/5n27475tvrev6woidcji.jpeg"
+        >
+          I'm gonna be honest: I hate data structure and algorithm interviews.
+          So I learned some techniques that made them bearable and went from
+          bombing them to landing a job at my dream company.
+          <strong> Read more →</strong>
+        </PostPreview>
+        <PostPreview
+          title="Get Hired: The Storytelling Resume"
+          url="https://dev.to/techdebtor/get-hired-the-storytelling-resume-19ln"
+          image="https://res.cloudinary.com/practicaldev/image/fetch/s--wTphUp0q--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://thepracticaldev.s3.amazonaws.com/i/d3j7dmwqim6w49i3h6qd.jpeg"
+        >
+          I got a new job this year and had to rewrite my resume while job
+          searching. I rewrote it with one thing in mind - I wanted to tell my
+          story and advocate for myself as an awesome developer.
+          <strong> Read more →</strong>
+        </PostPreview>
       </TechPostLayout>
     </PostContainer>
   );
